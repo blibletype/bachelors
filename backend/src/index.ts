2e1850f7fb482bfe8b832e6476f46b9c8892bebc
Maksym Koval <maxymkoval2510@gmail.com>
2024-05-30 03:15:35 +0300

    fix: added more logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,6 +28,13 @@ io.on('connection', (socket) => {
   });
 });
 
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
